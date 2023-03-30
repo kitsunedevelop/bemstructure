47822443b8609af9605a09b5ea64508e8fd30d5e
nikitakitsune <insta.nekitus@gmail.com>
2023-03-30 23:53:34 +0300

    v1

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+const fs = require('fs');
+const path = require('path');
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,10 +17,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('kitsune-bem-structure.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('kitsune-bem-structure.createBemStructure', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from BEM structure helper!');
+	
 	});
 
 	context.subscriptions.push(disposable);
